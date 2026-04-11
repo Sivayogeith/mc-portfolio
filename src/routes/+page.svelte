648f402917796cb3c87a66efc4247db98a338d73
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-04-11 10:47:43 +0530

    feat: added home page with links to builds, mods and farms

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+</script>
+
+<div class="flex flex-col justify-center items-center xl:h-screen xl:m-0 my-20">
+  <h1 class="text-3xl text-center mb-20 mx-5">sage's minecraft "portfolio"</h1>
+  <div class="grid xl:grid-cols-3 grid-cols-1 gap-5 mx-10">
+    <div
+      class="rounded-xl border border-sky-800 hover:scale-105 xl:pb-5 flex xl:flex-col md:flex-row flex-col"
+    >
+      <img
+        src="/ssmpBacapBase.png"
+        alt="minecraft builds cover"
+        class="rounded-bl-none rounded-t-xl md:rounded-l-xl md:rounded-r-none xl:rounded-bl-none xl:rounded-t-xl w-max md:w-1/2 xl:w-max"
+      />
+      <h1 class="text-2xl p-5">my minecraft builds</h1>
+    </div>
+    <div
+      class="rounded-xl border border-sky-800 hover:scale-105 xl:pb-5 flex xl:flex-col md:flex-row flex-col"
+    >
+      <img
+        src="/riceMod.png"
+        alt="minecraft builds cover"
+        class="rounded-bl-none rounded-t-xl md:rounded-l-xl md:rounded-r-none xl:rounded-bl-none xl:rounded-t-xl w-max md:w-1/2 xl:w-max"
+      />
+      <h1 class="text-2xl p-5">my minecraft mods and packs</h1>
+    </div>
+    <div
+      class="rounded-xl border border-sky-800 hover:scale-105 xl:pb-5 flex xl:flex-col md:flex-row flex-col"
+    >
+      <img
+        src="/endermenFarm.png"
+        alt="minecraft builds cover"
+        class="rounded-bl-none rounded-t-xl md:rounded-l-xl md:rounded-r-none xl:rounded-bl-none xl:rounded-t-xl w-max md:w-1/2 xl:w-max"
+      />
+      <h1 class="text-2xl p-5">list of the best farms</h1>
+    </div>
+  </div>
+</div>
