2f410f0040253c46b88e0dffbaa3b623b4402288
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2026-04-18 21:33:11 +0530

    feat: added dark mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,28 @@
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
   import { page } from "$app/state";
+  import SunIcon from "$lib/SunIcon.svelte";
+  import MoonIcon from "$lib/MoonIcon.svelte";
+  import { onMount } from "svelte";
 
   let { children } = $props();
+
+  let theme = $state("");
+
+  onMount(() => {
+    if (!localStorage.getItem("theme")) {
+      theme = window.matchMedia("(prefers-color-scheme: dark)")
+        ? "dark"
+        : "light";
+    } else {
+      theme = localStorage.getItem("theme") || "light";
+    }
+  });
+
+  $effect(() => {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  });
 </script>
 
 <svelte:head>
@@ -11,11 +31,26 @@
   <title>Sage's Minecraft Portfolio</title>
 </svelte:head>
 
-{#if page.url.pathname != "/"}
-  <div class="pt-5 flex justify-center flex-col">
-    <a class="text-3xl text-center" href="/">
-      <span class="hover:text-blue-900 dark:hover:text-emerald-500">sage's minecraft "portfolio"</span></a
+<div class="pt-5 flex flex-row justify-between w-full">
+  <div class="w-1/4"></div>
+  {#if page.url.pathname != "/"}
+    <a class="md:text-3xl text-2xl text-center text-bold" href="/">
+      <span class="hover:text-blue-900 dark:hover:text-emerald-500"
+        >sage's minecraft "portfolio"</span
+      ></a
     >
+  {/if}
+  <div class="w-1/4 text-end pe-5">
+    <button
+      onclick={() => (theme = theme == "dark" ? "light" : "dark")}
+      class="dark:bg-emerald-500 bg-blue-900 dark:text-black text-white rounded-full p-2 self-end"
+    >
+      {#if theme == "dark"}
+        <SunIcon />
+      {:else}
+        <MoonIcon />
+      {/if}
+    </button>
   </div>
-{/if}
+</div>
 {@render children()}
